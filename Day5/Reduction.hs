3b78554ba7affe98b91e227efd3c4b4d3fe8d7ee
ToF- <cthibaut@octo.com>
2019-02-08 17:05:24 +0100

    much better with foldr

--- Day5/Reduction.hs
+++ Day5/Reduction.hs
@@ -3,26 +3,15 @@ where
 import Data.Char
 import Data.List
 
--- reduce :: String -> String 
--- reduce (b:c:c':cs) | not (opposite b c) && opposite c c' = reduce (b:cs)
---                    | (opposite b c) = reduce (c':cs)
---                    | otherwise      = b: reduce (c:c':cs)
--- reduce (c:c':cs) | opposite c c' = reduce cs
---                  | otherwise = c : reduce (c':cs)
--- reduce s = s
-
-
 opposite :: Char -> Char -> Bool
 opposite c c' = toUpper c == toUpper c' && c /= c' 
 
-reduce :: String -> String
-reduce s | reduction s == s = s
-         | otherwise       = reduce (reduction s)
 
-reduction :: String -> String
-reduction (c:c':cs) | opposite c c' = reduction cs
-                    | otherwise     = c : reduction (c':cs)
-reduction s = s
+reduce :: String -> String
+reduce = foldr reduction ""
+    where
+    reduction c (d:ds) | opposite c d = ds
+    reduction c cs = c : cs 
 
 remove :: String -> Int
 remove s = head (sort 
