dfbdbf5d8e774ea72b2a20382fdb327e89579a51
ToF <cthibaut@octo.com>
2019-02-10 21:32:29 +0100

    still stuck

--- Day7/Specs.hs
+++ Day7/Specs.hs
@@ -3,29 +3,6 @@ import Steps
 import Data.Map as M
 import Data.List as L
 
-main = hspec $ do
-    let g = graph small
-    describe "graph" $ do
-        it "creates a graph of all steps" $ do
-            let l = L.sort $ M.toList $ g
-            l `shouldBe` 
-                [(A,[C]),(B,[A]),(D,[A]),(E,[B,D,F]),(F,[C])]
-
-    describe "target" $ do
-        it "tells the final step of the graph" $ do
-            target g `shouldBe` E
-
-    describe "previous steps" $ do
-        it "tells the previous steps not yet being done" $ do
-            previous g [E] E `shouldBe` [B,D,F]
-            previous g [E,B,D,F] B `shouldBe` [A]
-            previous g [E,B,D,F,A] D `shouldBe` []
-
-
-    describe "backward" $ do
-        it "tells all the previous steps" $ do
-            backward g E `shouldBe` []
-
 small = [(C,A)
         ,(C,F)
         ,(A,B)
@@ -33,3 +10,11 @@ small = [(C,A)
         ,(B,E)
         ,(D,E)
         ,(F,E)]
+
+main = hspec $ do
+    let g = graph small
+    describe "graph" $ do
+        it "creates a graph of all steps" $ do
+            let l = L.sort $ M.toList $ g
+            l `shouldBe` [(A,[C]),(B,[A]),(D,[A]),(E,[B,D,F]),(F,[C])]
+
--- Day7/Steps.hs
+++ Day7/Steps.hs
@@ -3,8 +3,12 @@ where
 import Data.Map as M
 import Data.List as L
 
+type Priority = Int
+type Required = [Node]
+type Task = (Priority,Required)
 type Edge = (Node,Node)
-type Graph = Map Node [Node]
+type Graph = Map Node Required
+type Priorities = Map Node Priority
 
 data Node = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z
     deriving (Eq,Ord,Show,Enum)
@@ -15,29 +19,6 @@ graph = L.foldl addEdge M.empty
     addEdge :: Graph -> Edge -> Graph
     addEdge map (from,to) = insertWith add to [from] map
         where
-        add :: [Node] -> [Node] -> [Node]
+        add :: Required -> Required -> Required
         add old new = L.nub (L.sort (old ++ new))
 
-target :: Graph -> Node
-target g = head (L.filter (\n -> not (n `elem` elems)) keys)
-    where
-    elems = L.nub (L.sort (L.concat (M.elems g)))
-    keys  = M.keys g
-
-previous :: Graph -> [Node] -> Node -> [Node]
-previous g al n = case n `M.lookup` g of
-    Just ns -> L.filter (\n -> not (n `elem` al)) ns
-    Nothing -> []
-
-backward :: Graph -> Node -> [[Node]]
-backward g n = backward' g [n] n
-    where
-    backward' :: Graph -> [Node] -> Node -> [[Node]]
-    backward' g al e = [n] : case previous g al n of
-        [] -> []
-        ns -> ns : case previous g (al ++ ns) (head ns) of
-            [] -> case previous g (al ++ ns) (head (tail ns)) of
-                [] -> []
-                ns -> [head ns] :  []
-            ns -> [head ns] :  []
-    
