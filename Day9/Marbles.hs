0df4b570d7d36bc4fda824d086faaad9a3c51126
ToF- <cthibaut@octo.com>
2019-02-18 21:02:11 +0100

    play N times work for N < 23

--- Day9/Marbles.hs
+++ Day9/Marbles.hs
@@ -1,11 +1,40 @@
 module Marbles
 where
+import Data.List
 
 type Marble = Int
-type Circle = [Marble]
+data Circle = Circle { 
+    marbles :: [Marble],
+    posCurrent :: Int,
+    lastInserted :: Int }
+    deriving (Eq)
+
+instance Show Circle where
+    show = showCircle 
     
 circle :: Circle
-circle = [0]
+circle = Circle [0] 1 0
 
 current :: Circle -> Marble
-current [0] = 0
+current c = (marbles c) !! ((posCurrent c)-1)
+
+next :: Int -> Int -> Int
+next size pos | pos >= size = 1 +  pos + 2 - (size+1)
+              | otherwise  = pos + 2
+ 
+add :: Circle -> Circle
+add (Circle [0] 1 0) = Circle [0,1] 2 1
+add (Circle marbles pos last) = let
+    pos' = next (length marbles) pos
+    ins = pos' - 1
+    marbles' = take ins marbles ++ return last' ++ drop ins marbles
+    last' = succ last
+    in Circle marbles' pos' last'
+
+showCircle :: Circle -> String
+showCircle (Circle ms pc _) = 
+    concat (intersperse " " (zipWith showMarble ms [1..]))
+    where
+    showMarble :: Marble -> Int -> String
+    showMarble m i | i == pc   = "(" ++ show m ++ ")"
+                   | otherwise =       show m
--- Day9/Specs.hs
+++ Day9/Specs.hs
@@ -3,7 +3,16 @@ import Marbles
 
 main = hspec $ do
     describe "marble circle" $ do
-        describe "has a current marble" $ do
-            it "initially it's marble 0" $ do
-                current circle `shouldBe` 0
+        it "can be show with current between ( )" $ do
+            let play n = showCircle (last (take (succ n) (iterate add circle)))
+            play 0 `shouldBe` "(0)"
+            play 1 `shouldBe` "0 (1)"
+            play 2 `shouldBe` "0 (2) 1"
+            play 3 `shouldBe` "0 2 1 (3)"
+            play 4 `shouldBe` "0 (4) 2 1 3"
+            play 5 `shouldBe` "0 4 2 (5) 1 3"
+            play 6 `shouldBe` "0 4 2 5 1 (6) 3"
+            play 7 `shouldBe` "0 4 2 5 1 6 3 (7)"
+            play 22 `shouldBe` "0 16 8 17 4 18 9 19 2 20 10 21 5 (22) 11 1 12 6 13 3 14 7 15"
+                
             
