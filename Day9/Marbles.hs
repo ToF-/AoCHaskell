ae603167a14111020b3972b1a916b7ceec69c53f
ToF- <cthibaut@octo.com>
2019-02-18 22:01:02 +0100

    stuck with a replacement

--- Day9/Marbles.hs
+++ Day9/Marbles.hs
@@ -6,14 +6,17 @@ type Marble = Int
 data Circle = Circle { 
     marbles :: [Marble],
     posCurrent :: Int,
-    lastInserted :: Int }
+    lastInserted :: Int,
+    scores :: [Int],
+    player :: Int
+    }
     deriving (Eq)
 
 instance Show Circle where
     show = showCircle 
     
-circle :: Circle
-circle = Circle [0] 1 0
+circle :: Int -> Circle
+circle n = Circle [0] 1 0 (replicate n 0) 0
 
 current :: Circle -> Marble
 current c = (marbles c) !! ((posCurrent c)-1)
@@ -21,20 +24,42 @@ current c = (marbles c) !! ((posCurrent c)-1)
 next :: Int -> Int -> Int
 next size pos | pos >= size = 1 +  pos + 2 - (size+1)
               | otherwise  = pos + 2
+
+prev :: Int -> Int -> Int
+prev size pos | pos <= 7   = size + pos - 7
+              | otherwise = pos - 7
  
 add :: Circle -> Circle
-add (Circle [0] 1 0) = Circle [0,1] 2 1
-add (Circle marbles pos last) = let
+add (Circle marbles pos last sc player) 
+    | ((last+1) `mod` 23) == 0 = let
+    
+    pos'     = prev (length marbles) pos
+    taken    = marbles !! (pos'-1)
+    marbles' = delete taken marbles
+    last'    = succ last
+    score    = (sc !! player) + last' + (marbles !! (pos'-1))
+    scores' =  replace player score sc
+    player'= (succ player) `mod` (length sc)
+    in Circle marbles' pos' last' (take 3 scores') player'
+
+add (Circle marbles pos last scores player) = let
     pos' = next (length marbles) pos
     ins = pos' - 1
     marbles' = take ins marbles ++ return last' ++ drop ins marbles
     last' = succ last
-    in Circle marbles' pos' last'
+    player' = (succ player') `mod` (length scores)
+    in Circle marbles' pos' last' scores player'
+
+replace :: Int -> a -> [a] -> [a]
+replace n elem elems = (take (n-1) elems) ++ [elem] ++ (drop n elems)
 
 showCircle :: Circle -> String
-showCircle (Circle ms pc _) = 
+showCircle (Circle ms pc _ _ _) = 
     concat (intersperse " " (zipWith showMarble ms [1..]))
     where
     showMarble :: Marble -> Int -> String
     showMarble m i | i == pc   = "(" ++ show m ++ ")"
                    | otherwise =       show m
+
+play :: Int -> Int -> Circle
+play n p = (last (take (succ n) (iterate add (circle p))))
--- Day9/Specs.hs
+++ Day9/Specs.hs
@@ -3,16 +3,23 @@ import Marbles
 
 main = hspec $ do
     describe "marble circle" $ do
+        let splay n = showCircle $ play n 9 
         it "can be show with current between ( )" $ do
-            let play n = showCircle (last (take (succ n) (iterate add circle)))
-            play 0 `shouldBe` "(0)"
-            play 1 `shouldBe` "0 (1)"
-            play 2 `shouldBe` "0 (2) 1"
-            play 3 `shouldBe` "0 2 1 (3)"
-            play 4 `shouldBe` "0 (4) 2 1 3"
-            play 5 `shouldBe` "0 4 2 (5) 1 3"
-            play 6 `shouldBe` "0 4 2 5 1 (6) 3"
-            play 7 `shouldBe` "0 4 2 5 1 6 3 (7)"
-            play 22 `shouldBe` "0 16 8 17 4 18 9 19 2 20 10 21 5 (22) 11 1 12 6 13 3 14 7 15"
+            splay 0 `shouldBe` "(0)"
+            splay 1 `shouldBe` "0 (1)"
+            splay 2 `shouldBe` "0 (2) 1"
+            splay 3 `shouldBe` "0 2 1 (3)"
+            splay 4 `shouldBe` "0 (4) 2 1 3"
+            splay 5 `shouldBe` "0 4 2 (5) 1 3"
+            splay 6 `shouldBe` "0 4 2 5 1 (6) 3"
+            splay 7 `shouldBe` "0 4 2 5 1 6 3 (7)"
+            splay 22 `shouldBe` "0 16 8 17 4 18 9 19 2 20 10 21 5 (22) 11 1 12 6 13 3 14 7 15"
+
+        it "has special rules for marble multiple of 23" $ do
+            splay 23 `shouldBe` "0 16 8 17 4 18 (19) 2 20 10 21 5 22 11 1 12 6 13 3 14 7 15"    
+
+        it "has n players with each a score" $ do
+            scores (circle 4) `shouldBe` [0,0,0,0]
+            scores (play 25 9) `shouldBe` []
                 
             
