6c71378aa835364cbabba3d23d7f9115d1c7cd45
ToF <cthibaut@octo.com>
2019-02-19 14:52:55 +0100

    can't figure this shift movement

--- Day9/CircularList.hs
+++ Day9/CircularList.hs
@@ -2,23 +2,17 @@ module CircularList
 where
 import Data.List
 
-data CircularList a = CList { pre :: [a], post :: [a] }
+data CircularList a = CList { anti :: [a], noon :: a, clock :: [a] } 
     deriving (Eq)
 
 instance Show a => Show (CircularList a) where
-    show cl@(CList pre post) = interSpace [showPre pre,showHead post,showPost post]
+    show cl@(CList anti noon clock) = interSpace 
+        [showSimpleList (reverse anti)
+        ,shownoon noon
+        ,showSimpleList (clock)]
 
-showHead :: Show a => [a] -> String
-showHead [] = "()"
-showHead (a:_) = "(" ++ show a ++ ")"
-
-showPre :: Show a => [a] -> String
-showPre [] = ""
-showPre as = showSimpleList (reverse as)
-
-showPost :: Show a => [a] -> String
-showPost l | length l < 2 = ""
-           | otherwise = showSimpleList (tail l)
+shownoon :: Show a => a -> String
+shownoon a = "(" ++ show a ++ ")"
 
 showSimpleList :: Show a => [a] -> String
 showSimpleList = interSpace . map show 
@@ -26,23 +20,14 @@ showSimpleList = interSpace . map show
 interSpace :: [String] -> String
 interSpace = concat . intersperse " " . filter (/= "")
 
-empty = CList [] []
+clist :: a -> CircularList a
+clist a = CList [] a []
 
 insert :: a -> CircularList a -> CircularList a 
-insert a (CList pre post) = CList pre (a:post)
+insert a (CList anti noon clock) = CList (noon:anti) a clock
 
-isEmpty :: CircularList a -> Bool
-isEmpty (CList pre post) = null pre && null post
-
-size :: CircularList a -> Int
-size (CList pre post) = length pre + length post
-
-shiftRight :: CircularList a -> CircularList a
-shiftRight cl@(CList pre post) | isEmpty cl || size cl == 1 = cl
-                               | otherwise = normalize (CList ((head post):pre) (tail post))
-
-normalize :: CircularList a -> CircularList a
-normalize cl | isEmpty cl = cl
-normalize (CList [] post) = CList [] post
-normalize (CList pre [])  = CList (init pre) (last pre:[])
-normalize cl = cl
+shift :: CircularList a -> CircularList a
+shift (CList [] noon []) = CList [] noon []
+shift (CList [a] b []) = CList [] a [b]
+shift (CList [] a [b]) = CList [a] b []
+shift (CList anti noon []) = CList (noon:(init anti)) (last anti) []
