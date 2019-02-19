22a7ccfc2a8328e7ba3f81af5dbddfc2bb57cd90
ToF- <cthibaut@octo.com>
2019-02-20 00:21:18 +0100

    Day 10 done

--- Day10/Main.hs
+++ Day10/Main.hs
@@ -1,15 +1,17 @@
 import Stars
 import Small
+import Large
 
 
-viewStars :: [Star] -> Int -> IO ()
-viewStars _ 0 = return ()
-viewStars st n | not (viewable (area st)) = viewStars (move 1 st) (pred n)
-viewStars st n = do
-    putStrLn ""
+viewStars :: [Star] -> Int -> Int -> IO ()
+viewStars _ 0 _ = return ()
+viewStars st n s | not (viewable (area st)) = viewStars (move 1 st) (pred n) (succ s)
+viewStars st n s = do
+    putStrLn (show s)
     putStrLn (view st)
     getLine
-    viewStars (move 1 st) (pred n)
+    viewStars (move 1 st) (pred n) (succ s)
 
 main = do
-    viewStars small 40
+    -- viewStars small 40
+    viewStars large 40000 0
