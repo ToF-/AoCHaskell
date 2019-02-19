7dd54b70adf8261ab5792e519292a7fc4d2dae24
ToF <cthibaut@octo.com>
2019-02-19 15:49:29 +0100

    got it for shifting the circle

--- Day9/Marbles.hs
+++ Day9/Marbles.hs
@@ -1,11 +1,30 @@
 module Marbles
 where
+import Data.List
 
-data Circle = Circle Int
+data Circle = Circle [Int] Int [Int]
     deriving Eq
 
 instance Show Circle where
-    show (Circle n) = "(" ++ show n ++ ")"
+    show (Circle ls h rs) = interSpace [showL (reverse ls), "(" ++ show h ++ ")", showL rs]
+
+showL = interSpace . map show
+
+interSpace :: [String] -> String
+interSpace = concat . intersperse " " . filter (/= "")
 
 circle :: Int -> Circle
-circle n = Circle n
+circle n = Circle [] n []
+
+add :: Int -> Circle -> Circle
+add n (Circle ls h rs) = Circle (h:ls) n rs
+
+right :: Circle -> Circle
+right (Circle [] h []) = Circle [] h []
+right (Circle ls h []) = Circle [] (head rs) (tail rs)
+    where rs = (reverse ls) ++ [h]
+right (Circle ls h rs) = Circle (h:ls) (head rs) (tail rs)
+
+play :: Int -> Circle -> Circle
+play n c = add n (right c)
+
--- Day9/Specs.hs
+++ Day9/Specs.hs
@@ -1,9 +1,40 @@
+-- [-] (0)
+-- [1]  0 (1)
+-- [2]  0 (2) 1 
+-- [3]  0  2  1 (3)
+-- [4]  0 (4) 2  1  3 
+-- [5]  0  4  2 (5) 1  3 
+-- [6]  0  4  2  5  1 (6) 3 
+-- [7]  0  4  2  5  1  6  3 (7)
+-- [8]  0 (8) 4  2  5  1  6  3  7 
 import Test.Hspec
 import Test.Hspec.Core.QuickCheck
 import Test.QuickCheck
 import Marbles
 
+list ns = foldl (\c n -> add n c) i ns
+i = circle 0
 main = hspec $ do
     describe "a circle " $ do
         it "is created with a head" $ do
-            show (circle 0) `shouldBe` "(0)"
+            show i `shouldBe` "(0)"
+
+        it "can insert a new marble after the head" $ do
+            show (add 1 i) `shouldBe` "0 (1)"
+
+        it "can insert several new marbles" $ do
+            show (add 2 (add 1 i)) `shouldBe` "0 1 (2)"
+            show (list [1..3]) `shouldBe` "0 1 2 (3)"
+
+        it "can shift the head to the right" $ do
+            show (right i) `shouldBe` "(0)"
+            show (right (add 1 i)) `shouldBe` "(0) 1"
+            show (right (list [1..3])) `shouldBe` "(0) 1 2 3"
+
+        it "playing is shift once then adding" $ do
+            show (play 1 i) `shouldBe` "0 (1)"
+            show (play 2 (play 1 i)) `shouldBe` "0 (2) 1"
+            show (play 3 (play 2 (play 1 i))) `shouldBe` "0 2 1 (3)"
+            show (foldl (\c n -> play n c) i [1..7]) `shouldBe` "0 4 2 5 1 6 3 (7)"
+            show (foldl (\c n -> play n c) i [1..22])`shouldBe`
+                "0 16 8 17 4 18 9 19 2 20 10 21 5 (22) 11 1 12 6 13 3 14 7 15"
