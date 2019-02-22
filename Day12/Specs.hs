46ee21f1be08724bae2d540e8cda033991a5e59f
ToF- <cthibaut@octo.com>
2019-02-22 16:02:20 +0100

    patterns and pot numbers

--- Day12/Specs.hs
+++ Day12/Specs.hs
@@ -1,26 +1,39 @@
 import Test.Hspec
+import Sustain
 
-notes = ["...##"
-        ,"..#.."
-        ,".#..."
-        ,".#.#."
-        ,".#.##"
-        ,".##.."
-        ,".####"
-        ,"#.#.#"
-        ,"#.###"
-        ,"##.#."
-        ,"##.##"
-        ,"###.."
-        ,"###.#"
-        ,"####."]
+notes = [("...##",'#')
+        ,("..#..",'#')
+        ,(".#...",'#')
+        ,(".#.#.",'#')
+        ,(".#.##",'#')
+        ,(".##..",'#')
+        ,(".####",'#')
+        ,("#.#.#",'#')
+        ,("#.###",'#')
+        ,("##.#.",'#')
+        ,("##.##",'#')
+        ,("###..",'#')
+        ,("###.#",'#')
+        ,("####.",'#')
+        ,(".....",'.')]
 
 main = hspec $ do
-   describe "a pattern " $ do
-    it "tells if a plant will grow or not" $ do
-         "...##" `elem` notes `shouldBe` True 
+    describe "a pattern " $ do
+        it "tells if a plant will grow or not" $ do
+                     "...##" `find` notes `shouldBe` True
+                     "#####" `find` notes `shouldBe` False
+                     "....." `find` notes `shouldBe` False
+    describe "numberPots" $ do
+        it "numbers the pots and add 5 empty pots before and after a line" $ do
+            numberPots "##.#.##.#" `shouldBe`
+                [('.',-5),('.',-4),('.',-3),('.',-2),('.',-1),('#',0),('#',1),('.',2),('#',3),('.',4),('#',5),('#',6),('.',7),('#',8),('.',9),('.',10),('.',11),('.',12),('.',13)]
+                
+            
+            
 
-    it "tells if several plants will grow or not" $ do
-        let initial = "...#..#.#..##......###...###..........."
-        let next    = "...#...#....#.....#..#..#..#..........."
-        sustain notes initial  `shouldBe` next 
+
+
+--     it "tells if several plants will grow or not" $ do
+--         let initial = "...#..#.#..##......###...###..........."
+--         let next    = "...#...#....#.....#..#..#..#..........."
+--         sustain notes initial  `shouldBe` next 
--- Day12/Sustain.hs
+++ Day12/Sustain.hs
@@ -1,5 +1,13 @@
 module Sustain
 where
+import Data.Maybe
 
-sustain :: [String] -> String -> String
-sustain notes initial
+type Pattern = String
+type Notes = [(Pattern,Char)]
+find :: Pattern -> Notes -> Bool
+find p notes = maybe False (=='#') (lookup p notes)
+
+
+numberPots :: Pattern -> [(Char,Int)]
+numberPots p = ("....." ++ p ++ ".....") `zip` [-5..]
+
