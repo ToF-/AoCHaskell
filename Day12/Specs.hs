dec517e66e3ecffeb5ffee104752fa5a850b6703
ToF <cthibaut@octo.com>
2019-02-23 11:55:49 +0100

    Day 12 Part 1 done

--- Day12/Specs.hs
+++ Day12/Specs.hs
@@ -17,6 +17,32 @@ notes = ["...##"
         ,"###.#"
         ,"####."]
 
+--                 1         2         3     
+--       0         0         0         0     
+-- 0: ...#..#.#..##......###...###...........
+-- 1: ...#...#....#.....#..#..#..#...........
+-- 2: ...##..##...##....#..#..#..##..........
+-- 3: ..#.#...#..#.#....#..#..#...#..........
+-- 4: ...#.#..#...#.#...#..#..##..##.........
+-- 5: ....#...##...#.#..#..#...#...#.........
+-- 6: ....##.#.#....#...#..##..##..##........
+-- 7: ...#..###.#...##..#...#...#...#........
+-- 8: ...#....##.#.#.#..##..##..##..##.......
+-- : ...##..#..#####....#...#...#...#.......
+--10: ..#.#..#...#.##....##..##..##..##......
+--11: ...#...##...#.#...#.#...#...#...#......
+--12: ...##.#.#....#.#...#.#..##..##..##.....
+--13: ..#..###.#....#.#...#....#...#...#.....
+--14: ..#....##.#....#.#..##...##..##..##....
+--15: ..##..#..#.#....#....#..#.#...#...#....
+--16: .#.#..#...#.#...##...#...#.#..##..##...
+--17: ..#...##...#.#.#.#...##...#....#...#...
+--18: ..##.#.#....#####.#.#.#...##...##..##..
+--19: .#..###.#..#.#.#######.#.#.#..#.#...#..
+--20: .#....##....#####...#######....#.#..##.
+--
+--         0    0    1    0    2   
+--         0    5    0    5    0     
 initial = "#..#.#..##......###...###"
 
 listOfPlants :: Gen String
@@ -34,8 +60,54 @@ main = hspec $ do
             plants [0,0,3,5,8,9,16,17,18,22,23,24] `shouldBe` initial
 
     describe "offset" $ do
-        it "describe a list of plants with an number of empty pots ahead" $ do
-            offset (-3) [0,0] `shouldBe` "...#"
+        it "describe a list of 5 plants at the given position" $ do
+            offset (-3) [0,0]     `shouldBe` "...#."
+            offset (-3) [-2,-2,0] `shouldBe` ".#.#."
+            offset 0 [3,3]        `shouldBe` "...#."
+            offset 3 [3,3]        `shouldBe` "#...."
+            let p = pattern 0 initial
+            offset 0 p `shouldBe` "#..#."
+            offset 1 p `shouldBe` "..#.#"
+            offset 2 p `shouldBe` ".#.#."
+            offset 5 p `shouldBe` "#..##"
+
+    describe "sustain" $ do
+        let p = pattern 0 initial
+        it "sustainAt if a list of plant can sustain in a given pot" $ do
+            sustainAt [0,0] notes (-5) `shouldBe` Nothing
+            sustainAt [0,0] notes (-2) `shouldBe` Just 0
+            sustainAt [0,1,3,4] notes 0 `shouldBe` Just 2
+            sustainAt p notes 22 `shouldBe` Just 24
+        it "sustain tells all the plants that grew from a pattern" $ do
+            sustain [0,0] notes  `shouldBe` [0,0,1]
+            sustain [0,0,1] notes  `shouldBe` [-1,-1,1]
+            sustain p notes `shouldBe` [0,0,4,9,15,18,21,24]
+            plants (sustain p notes) `shouldBe` "#...#....#.....#..#..#..#"
+        it "sustainN tells all the plants that grew after N generations" $ do
+            sustainN 1 p notes `shouldBe` sustain p notes
+            sustainN 2 p notes `shouldBe` sustain (sustain p notes) notes
+            plants (sustainN 20 p notes) `shouldBe` "#....##....#####...#######....#.#..##"
+
+    describe "sum pots" $ do
+        let p = pattern 0 initial
+        it "sum the pot numbers of a given pattern" $ do
+            sumPots (sustainN 20 p notes) `shouldBe` 325
+        let notes = ["#####" 
+                    ,"#.##."
+                    ,"##.#." 
+                    ,"#..#." 
+                    ,"##..." 
+                    ,"#...#" 
+                    ,".#..#" 
+                    ,".#..." 
+                    ,"##.##" 
+                    ,"...##" 
+                    ,"..###" 
+                    ,".##.#" 
+                    ,"##..#"]
+        let q = pattern 0 "##.......#.######.##..#...#.#.#..#...#..####..#.##...#....#...##..#..#.##.##.###.##.#.......###....#"
+        it "finds the pot numbers for the puzzle" $ do
+            sumPots (sustainN 20 q notes) `shouldBe` 2840
 
     describe "plants and pattern" $ do
         it "are symmetrical" $ forAll listOfPlants $ \s -> forAll (choose (-5,5)) $ 
--- Day12/Sustain.hs
+++ Day12/Sustain.hs
@@ -3,7 +3,7 @@ where
 import Data.Maybe
 
 type Pattern = [Int]
-type Note = (String,Char)
+type Note = String
 
 pattern :: Int -> String -> Pattern
 pattern i s = i : map snd (filter (\(c,n) -> c=='#') (s `zip` [i..]))
@@ -17,4 +17,24 @@ plants p = plant' (head p) (tail p)
                     | i > n = error "ill formed pattern"
                 
 offset :: Int -> Pattern -> String
-offset o (n:ns) = (replicate (n-o) '.') ++ plants (n:ns)
+offset o (n:ns) | o < n = take 5 ((replicate (n-o) '.') ++ plants (n:ns) ++ (repeat '.'))
+offset o (n:ns)         = take 5 (drop (o - n) (plants (n:ns) ++ (repeat '.')))
+
+sustainAt :: Pattern -> [Note] -> Int -> Maybe  Int
+sustainAt p notes o = case any (== (offset o p)) notes of
+    False -> Nothing
+    True -> Just (o+2)
+
+sustain :: Pattern -> [Note] -> Pattern
+sustain p notes = case catMaybes (map (\o -> sustainAt p notes o) [start..end]) of
+    [] -> []
+    (n:ns) -> (n:n:ns)
+    where
+    start = head p - 5
+    end = last p + 5
+
+sustainN :: Int -> Pattern -> [Note] -> Pattern
+sustainN n p notes = foldl (\acc _ -> sustain acc notes) p [1..n]
+
+sumPots :: Pattern -> Int
+sumPots (n:ns) = sum ns
