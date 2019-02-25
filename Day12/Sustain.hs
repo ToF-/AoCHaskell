891820a38ed92393f3b2b56ea5d0588979c3acc2
ToF <cthibaut@octo.com>
2019-02-25 23:37:55 +0100

    arrays

--- Day12/Sustain.hs
+++ Day12/Sustain.hs
@@ -1,21 +1,44 @@
 module Sustain
 where
-import Data.List
+import Data.List as L
+import Data.Array as A
 
-type Pattern = (Int, String)
+type Pattern = Array Integer Char
+type Note = Array Integer Char
 
-extendLeft l (n,s) = (n-l,replicate l '.' ++ s)
-extendRight r (n,s) = (n,s ++ replicate r '.')
-normalize (n,[]) = (n,[])
-normalize (n,'#':s) = (n,'#': trimRight s)
-    where trimRight = reverse . dropWhile (=='.') . reverse
-normalize (n,'.':s) = normalize (succ n,s)
+notes :: [String] -> Note
+notes ss = map (listArray (0,4)) ss
+initial s = listArray (0,length s) s
 
-sustain :: Pattern -> [String] -> Pattern
-sustain (n,s) notes = let
-    ps = transpose ["...."++s,"..."++s++".",".."++s++"..","."++s++"...",s++"...."]
-    match t | t `elem` notes = '#'
-            | otherwise = '.'
-    in normalize (n-2, map match ps)
+extendLeft l a = listArray (m-l,n) (replicate l '.' ++ elems a)
+    where (m,n) = bounds a
+
+extendRight r a = listArray (m,n+r) (elems a ++ replicate r '.')
+    where (m,n) = bounds a
+
+normalize a | bounds a == (0,0) = a
+
+normalize a | a!m == '.' = listArray (succ m,n) (tail (elems a))
+    where (m,n) = bounds a
+
+normalize a | a!m == '#' = listArray (m,m+l) s
+    where
+    (m,n) = bounds a 
+    s = reverse (dropWhile (=='.') (reverse (elems a)))
+    l = length s
+
+
+sustain :: Pattern -> [Array Int Char] -> Pattern
+sustain a notes = let
+    a' = extendRight 4 (extendLeft 4 a)
+    (m,n) = bounds a
+    ixs = range (bounds a')
+    jxs = range (0,4)
+    match i note = and [a'!i+j == note!j | j <- jxs]
+    matches i = or (map (match i) notes)
+    s = map (represent . matches) ixs
+    represent True = '#'
+    represent False = '.'
+    in normalize (listArray (m-2,n+2) s)
 
 times n f a = foldr ($) a (replicate n f)
