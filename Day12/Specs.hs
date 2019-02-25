7d0e705a2b65b82c0a6a7b612d492af3776a82a9
ToF- <cthibaut@octo.com>
2019-02-25 18:20:34 +0100

    too long

--- Day12/Specs.hs
+++ Day12/Specs.hs
@@ -27,6 +27,7 @@ notes = ["...##"
 --20: .#....##....#####...#######....#.#..##.
 --
 
+initial = (0,"#..#.#..##......###...###") :: Pattern
 initialPattern :: Gen (Int,String)
 initialPattern = fmap ((\s -> (0,s)).(++"#").('#':)) $ listOf1 (elements "#.")
 
@@ -38,4 +39,13 @@ main = do
                  let
                     p' = extendRight r (extendLeft l p)
                  in normalize p' == p
+    hspec $ do
+        describe "sustain" $ do
+            it "tells which pots will contain a plant according to notes" $ do
+                let p = (0,"#..#.#..##......###...###")
+                sustain p notes `shouldBe` (0,"#...#....#.....#..#..#..#")
+
+            it "can be repeated" $ do
+                times 20 (flip sustain notes) initial `shouldBe`  
+                 (-2,"#....##....#####...#######....#.#..##")
         
--- Day12/Sustain.hs
+++ Day12/Sustain.hs
@@ -1,5 +1,6 @@
 module Sustain
 where
+import Data.List
 
 type Pattern = (Int, String)
 
@@ -9,3 +10,12 @@ normalize (n,[]) = (n,[])
 normalize (n,'#':s) = (n,'#': trimRight s)
     where trimRight = reverse . dropWhile (=='.') . reverse
 normalize (n,'.':s) = normalize (succ n,s)
+
+sustain :: Pattern -> [String] -> Pattern
+sustain (n,s) notes = let
+    ps = transpose ["...."++s,"..."++s++".",".."++s++"..","."++s++"...",s++"...."]
+    match t | t `elem` notes = '#'
+            | otherwise = '.'
+    in normalize (n-2, map match ps)
+
+times n f a = foldr ($) a (replicate n f)
