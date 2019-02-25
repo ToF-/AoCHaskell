d8b91b1735947a2ee6d932288bcfc1c81e6072a8
ToF- <cthibaut@octo.com>
2019-02-25 17:38:15 +0100

    start again. extendLeft, extendRight, normalize

--- Day12/Specs.hs
+++ Day12/Specs.hs
@@ -27,50 +27,15 @@ notes = ["...##"
 --20: .#....##....#####...#######....#.#..##.
 --
 
-initialPattern :: Gen String
-initialPattern = fmap ('#':) $ listOf1 (elements "#.")
+initialPattern :: Gen (Int,String)
+initialPattern = fmap ((\s -> (0,s)).(++"#").('#':)) $ listOf1 (elements "#.")
 
-main = hspec $ do
-    describe "initial" $ do
-        describe "yields a pattern" $ do
-            it "when given #" $ initial "#"  `shouldBe` Pattern 0 1 1
-            it "when given #.#" $ initial "#.#"  `shouldBe` Pattern 0 3 5
-            it "ignoring . on left" $ initial ".###" `shouldBe` Pattern 0 3 7
-            it "ignoring . on right" $ initial "#..#." `shouldBe` Pattern 0 4 9
-    describe "a pattern" $ do
-        describe "can be extended on the right" $ do
-            it "by a certain number" $ do
-                extendRight (initial "#..#") 2 `shouldBe` Pattern 0 6 36
-        describe "can be extended on the left" $ do
-            it "by a certain number" $ do
-                extendLeft (initial "#..#") 2 `shouldBe` Pattern (-2) 6 9
-        describe "can be represented" $ do
-            it "in normal size with smallest value" $ represent (initial "#") `shouldBe` "#"
-            it "in normal size with large value" $ represent (initial "#.##") `shouldBe` "#.##"
-            it "in extended right" $ do represent (Pattern 0 6 36) `shouldBe` "#..#.."
-            it "in extended left" $ do represent (Pattern (-2) 8 36) `shouldBe` "..#..#.."
-
-        describe "can be normalized" $ do
-            it "to remove extensions on the right" $ normalize (Pattern 0 6 36) `shouldBe` Pattern 0 4 9
-            it "to remove extensions on the left" $ normalize (Pattern (-2) 8 36) `shouldBe` Pattern 0 4 9
-
-            it "always" $ forAll initialPattern $ \s ->
-                forAll (choose (1,10)) $ \r -> 
-                    forAll (choose (1,10)) $ \l ->
-                        let i = initial (take 30 s)
-                            p = extendLeft (extendRight i r) l
-                        in normalize p == i
-    describe "note" $ do
-        it "is a pattern of 5 plants" $ note "#..#." `shouldBe` Pattern 0 5 18
-        it "can be extended many times to attain a given size" $ do
-            map represent (extensions (note "#..#.") 7) `shouldBe`
-                ["#..#.","#..#..","#..#..."]
-    describe "match" $ do
-        it "tells if a note matches a pattern at the last position" $ do
-            ((initial "#..#.#..#..#") `match` (note ".#..#")) `shouldBe` True
-            ((initial "#..#.#..#..#") `match` (extendRight (note ".#..#") 1)) `shouldBe` False
-    describe "matches" $ do
-         it "tells if a note matches a pattern in what positions" $ do
-            ((initial "#..#.#..#..#") `matches` (note ".#..#")) `shouldBe` [1,6,9]
-            ((initial "#..#.#..#..#") `matches` (note "....#")) `shouldBe` [1,6,9]
-
+main = do 
+    quickCheck $ forAll initialPattern $ \(n,s) ->  head s == '#' && last s == '#'
+    quickCheck $ forAll initialPattern $ \p -> 
+        forAll (choose (1,4)) $ \l ->
+            forAll (choose (1,4)) $ \r ->
+                 let
+                    p' = extendRight r (extendLeft l p)
+                 in normalize p' == p
+        
--- Day12/Sustain.hs
+++ Day12/Sustain.hs
@@ -1,63 +1,11 @@
 module Sustain
 where
-import Data.Bits
 
-type Start = Int
-type Count = Int
-type Pots = Integer
-data Pattern = Pattern Start Count Pots
-    deriving (Eq, Show)
+type Pattern = (Int, String)
 
-addBits "" = error "ill formed initial pattern"
-addBits s = foldl addPot (Pattern 0 0 0) s
-    where
-    addPot (Pattern s c pots) '#' = Pattern s (succ c) ((pots `shiftL` 1) `setBit` 0)
-    addPot (Pattern s c pots) '.' = Pattern s (succ c) (pots `shiftL` 1)
-
-initial :: String -> Pattern
-initial = addBits . trimRight . trimLeft
-    where
-
-    trimRight = reverse . trimLeft . reverse
-
-    trimLeft = dropWhile (=='.')
-
-extendRight :: Pattern -> Count -> Pattern
-extendRight (Pattern s c pots) n = Pattern s (c + n) (pots `shiftL` n)
-
-extendLeft :: Pattern -> Count -> Pattern
-extendLeft (Pattern s c pots) n = Pattern (s-n) (c + n) pots
-
-represent :: Pattern -> String
-represent (Pattern s c pots) = replicate (c-length (repr)) '.' ++ repr
-    where
-    repr = reverse (addChar pots) 
-    addChar 0 = "."
-    addChar 1 = "#"
-    addChar pots | odd pots  = '#' : addChar (pots `shiftR` 1)
-                 | otherwise = '.' : addChar (pots `shiftR` 1)
-
-normalize :: Pattern -> Pattern 
-normalize = normalizeLeft . normalizeRight 
-    where
-    normalizeRight p@(Pattern _ 0 _) = p
-    normalizeRight p@(Pattern s c pots) | odd pots  = p
-                                        | otherwise = normalizeRight (Pattern s (pred c) (pots `shiftR` 1))
-    normalizeLeft p@(Pattern _ 0 _) = p
-    normalizeLeft p@(Pattern s c pots)  | pots `testBit` (pred c) = p 
-                                        | otherwise = normalizeLeft (Pattern (succ s) (pred c) pots)
-
-note :: String -> Pattern
-note = addBits
-
-extensions :: Pattern -> Int -> [Pattern]
-extensions p@(Pattern _ c _) n = map (\n -> p `extendRight` n) [0..n-c]
-
-match :: Pattern -> Pattern -> Bool
-match (Pattern _ _ pots) (Pattern _ _ note) = (pots `xor` note) == complement note
-
-matches :: Pattern -> Pattern -> [Int]
-matches p note = 
-    let p'@(Pattern s c pots) = (p `extendRight` 4) `extendLeft` 4
-        xs = note `extensions` c 
-    in reverse $ map (\(Pattern _ c' _) -> c-c'-2) $  filter (\x -> p' `match` x) xs
+extendLeft l (n,s) = (n-l,replicate l '.' ++ s)
+extendRight r (n,s) = (n,s ++ replicate r '.')
+normalize (n,[]) = (n,[])
+normalize (n,'#':s) = (n,'#': trimRight s)
+    where trimRight = reverse . dropWhile (=='.') . reverse
+normalize (n,'.':s) = normalize (succ n,s)
